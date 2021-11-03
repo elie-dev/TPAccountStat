c6cf9a1a501c9e5272c0781f4d737a43ce2d3947
Max_Le_Jeune <mlejeunepro@gmail.com>
2021-11-03 16:29:22 +0100

    :construction: verification + bull tab data

--- pages/transaction.vue
+++ pages/transaction.vue
@@ -7,12 +7,12 @@
       class="elevation-1"
     >
       <template v-slot:item.categorie="{ item }">
-        <v-chip
-          :color="getCategorie(item.categorie)"
-          dark
-        >
-          {{ item.calories }}
+        <v-chip :color="getCategorie(item.categorie)" dark>
+          {{ item.categorie }}
         </v-chip>
+      </template>
+      <template v-slot:item.montant="{ item }">
+          {{ item.montant }} €
       </template>
     </v-data-table>
 
@@ -37,45 +37,80 @@
           <v-card-title>
             <span class="text-h5">Ajouter une transaction</span>
           </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-btn-toggle borderless v-model="categorie">
-                  <v-btn value="revenue">
-                    <span class="hidden-sm-and-down">Revenue</span>
-                  </v-btn>
 
-                  <v-btn value="depense">
-                    <span class="hidden-sm-and-down">Dépense</span>
-                  </v-btn>
-                </v-btn-toggle>
-                <v-col cols="12">
-                  <v-text-field v-model="name" label="nom" type='text' required></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field v-model="tag" label="sous-categorie" type='text' required></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field v-model="montant" label="montant" type="number" required></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="auto">
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-text-field v-model="date" label="Picker in menu" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
-                    </template>
 
-                    <v-date-picker v-model="date" no-title scrollable>
-                      <v-spacer></v-spacer>
-                      <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-                      <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-                    </v-date-picker>
+          <v-form  ref="form"
+                   v-model="valid"
+                   lazy-validation>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-btn-toggle
+                    borderless
+                    v-model="categorie"
+                  >
+                    <v-btn value="revenue">
+                      <span class="hidden-sm-and-down">Revenue</span>
+                    </v-btn>
+                    <v-btn value="depense">
+                      <span class="hidden-sm-and-down">Dépense</span>
+                    </v-btn>
+                  </v-btn-toggle>
+                  <v-col cols="12">
+                    <v-text-field
+                      :rules="nameRules"
+                      v-model="name"
+                      label="nom"
+                      type='text'
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      :rules="categorieRules"
+                      v-model="tag"
+                      label="sous-categorie"
+                      type='text'
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      :rules="montantRules"
+                      v-model="montant"
+                      label="montant"
+                      type="number"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="auto">
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                          :rules="dateRules"
+                          v-model="date"
+                          label="Picker in menu"
+                          prepend-icon="mdi-calendar"
+                          readonly v-bind="attrs"
+                          v-on="on"
+                        >
+                        </v-text-field>
+                      </template>
 
-                  </v-menu>
-                </v-col>
+                      <v-date-picker v-model="date" no-title scrollable>
+                        <v-spacer></v-spacer>
+                        <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
+                        <v-btn text color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+                      </v-date-picker>
 
-              </v-row>
-            </v-container>
-          </v-card-text>
+                    </v-menu>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+          </v-form>
+
+
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="warning" text @click="dialog = false">
@@ -96,12 +131,14 @@
 
 import { ACTIONS } from '~/store/transaction'
 import { BUS_ACTIONS } from '~/store/bus'
+import AlertComponent from '~/components/AlertComponent.vue'
 
 export default {
+  components: { AlertComponent },
   async mounted() {
      try {
         await this.$store.dispatch(ACTIONS.GET_TRANSACTION)
-        
+
         this.setData()
       } catch (error) {
         console.log(error)
@@ -116,11 +153,17 @@ export default {
     dialog: false,
     date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
     menu: false,
-    modal: false,
-    menu2: false,
+    valid: true,
+    isInvalid: false,
 
     name: '',
+    nameRules: [
+      v => !!v || 'Un nom est obligatoire',
+    ],
     montant: '',
+    montantRules: [
+      v => !!v || 'Un montant est obligatoire',
+    ],
     tag: '',
     tagRules: [
       v => !!v || 'Un tag est obligatoire',
@@ -147,24 +190,29 @@ export default {
   }),
 
   methods: {
+
     getCategorie (categorie) {
       if (categorie === "depense") return 'red'
       else return 'green'
     },
 
-    setData () {
-      //console.log(this.$store.state.transaction.transactions)
-      //this.desserts = this.$store.state.transaction.transactions;
-    },
-
     async addTransaction() {
-      this.dialog = false
-      const data = {
-        categorie : this.categorie,
-        name : this.name,
-        tag : this.tag,
-        montant : this.montant,
-        date : this.date
+      if (this.name == '' || this.categorie == '' || this.montant == '') {
+        this.$store.dispatch(
+          BUS_ACTIONS.SET_MESSAGE,
+          'Tous les champs sont requis.'
+        )
+
+        return
+      }
+
+        this.dialog = false
+        const data = {
+          categorie : this.categorie,
+          name : this.name,
+          tag : this.tag,
+          montant : this.montant,
+          date : this.date
       }
 
       try {
