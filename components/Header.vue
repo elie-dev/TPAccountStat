3946671f7fa707292ee2980430b3a6e7b21a9a72
Maxime LE JEUNE <mlejeunepro@gmail.com>
2022-01-05 15:40:46 +0100

    wip final

--- components/Header.vue
+++ components/Header.vue
@@ -115,7 +115,7 @@
 .head {
   z-index: 8;
   /* background-color: #778DA9!important; */
-  background-color: yellow!important;
+  background-color: green!important;
   vertical-align: middle;
 }
 .sideList {
